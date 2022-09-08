f1ac24b927c773e19bf4a82aa1d439d8bc3e826b
TZCai <13818704679@163.com>
2022-09-08 07:33:09 +0200

    update action energy consumptions
    Former-commit-id: c156b810dcad8450572fe41ff9cae3272dcb8841

--- env/config.py
+++ env/config.py
@@ -4,7 +4,7 @@ from traffic import TrafficType
 # simulation parameters
 timeStep = 1e-3  # size of a simulation step in seconds
 startTime = 0  # time in a week in seconds (peak time: 307800 = Thu 13:30)
-actionInterval = 20  # sim steps between two actions (1 env step = 10 sim steps)
+actionInterval = 20  # sim steps between two actions (1 env step = 20 sim steps)
 accelRate = 600  # 1h in real time = 6s (600 env steps) in simulation time
 statsInterval = 5  # env steps between two stats printouts
 episodeTimeLen = 60 * 60 * 24 * 7  # the duration of an episode in simulation (seconds in a week)
