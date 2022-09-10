e2973487de2dfa336b27355f663e8d85c99548fc
tztsai <13818704679@163.com>
2022-09-10 11:36:21 +0000

    update train.sh
    Former-commit-id: 4336aa82a27baabc18f0a9f4ac2e719cef1af7f2 [formerly abed26d3d59512b17d2316ad3401a61d7a5a9625]
    Former-commit-id: 0e4b0ce526ba0e636270fd34553a3962fef66a61

---
+++ network/.ipynb_checkpoints/config-checkpoint.py
@@ -0,0 +1,44 @@
+import numpy as np
+from utils import dB2lin, lin2dB
+
+renderMode = 'none'
+
+# base station params
+numBS = 7
+interBSDist = 200  # the distance between two adjacent BSs
+cellRadius = 500  # the radius of a hexagon cell in meters
+antennaPower = 0.2  # maximum BS power in watts (src: Energy Saving Game for Massive MIMO)
+bsFrequency = 1.9e9  # carrier frequency in Hz
+antennaGain = 18  # power gain in dB of each antenna of a BS
+# feederLoss = 1  # feeder loss in dB (XXX: included in antennaGain)
+numAntennas = 64  # max number of antennas
+bandWidth = 20e6  # communication bandwidth in Hz
+bsHeight = 30  # height difference between a BS and a user in meters
+powerAllocWeights = [128, 2, 1]  # weights of the power allocation
+# powerAllocWeights = [1, 1, 1]  # weights of the power allocation
+antennaSwitchOpts = [-16, -4, 0, 4, 16]
+sleepDiscounts = [1, 0.195, 0.114, 0.076]
+wakeupDelays = [0, 1e-3, 1e-2, 1]
+antSwitchEnergy = 0.1  # energy consumption of switch per antenna in Joules
+sleepSwitchEnergy = [0, 0.1, 0.2, 2]  # energy consumption of switching sleep mode in Joules
+handoffEnergy = 0.06  # energy consumption of a UE handoff in Joules
+
+# channel model params
+noiseVariance = bandWidth * dB2lin(-174 - 30 + 7)
+# thermal noise variance
+
+# user equipment params
+ueHeight = 1.5  # height of a user equipment in meters
+
+# default network configuration
+areaSize = np.array([4, 4]) * interBSDist
+bsPositions = np.vstack([
+    np.array(
+        [[areaSize[0]/2 + interBSDist * np.sqrt(3) * np.cos(a + np.pi/6),
+          areaSize[1]/2 + interBSDist * np.sqrt(3) * np.sin(a + np.pi/6)]
+         for a in np.linspace(0, 2*np.pi, 7)[:-1]]),
+    # np.array(
+    #     [[AREA[0]/2 + R * 3 * np.cos(a),
+    #       AREA[1]/2 + R * 3 * np.sin(a)]
+    #      for a in np.linspace(0, 2*np.pi, 13)[:-1]]),
+    areaSize[None, :] / 2])
