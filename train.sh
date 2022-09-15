f2adce02947f5253ffdea9a2512bb052a22507c9
TZCai <13818704679@163.com>
2022-09-15 22:58:52 +0200

    update channel and reward params
    Former-commit-id: 787d55be592c546ba1fefa12aff5f01e3f829685 [formerly a565957321336653c335bc54ef8294eee2355a58]
    Former-commit-id: 44787b46ef828f4a66b39ca12e585890aec10e31

--- train.sh
+++ train.sh
@@ -6,18 +6,19 @@ traffic_type="A"
 accelerate=600
 n_training_threads=4
 n_rollout_threads=42
-num_env_steps=$((50400 * 120))  # steps_per_episode * episodes
+num_env_steps=$((50400 * 100))  # steps_per_episode * episodes
 experiment="check"
 
 algo="mappo"
 gain=0.01
 lr=7e-4
 critic_lr=7e-4
-ppo_epoch=10
+ppo_epoch=15
 num_mini_batch=1
 
-# w_pc=1.0
-# w_drop=0.1
+w_pc=1.0
+w_drop=1.0
+w_delay=2.0
 
 log_interval=1
 
@@ -28,5 +29,5 @@ echo "algo is ${algo}, traffic type is ${traffic_type}, max seed is ${seed_max}"
 for seed in `seq ${seed_max}`;
 do
     echo "seed is ${seed}:"
-    CUDA_VISIBLE_DEVICES=0 python train.py $@ --algorithm_name ${algo} --experiment_name ${experiment} --traffic_type ${traffic_type} --accelerate ${accelerate} --seed ${seed} --n_training_threads ${n_training_threads} --n_rollout_threads ${n_rollout_threads} --num_mini_batch ${num_mini_batch} --num_env_steps ${num_env_steps} --ppo_epoch ${ppo_epoch} --gain ${gain} --lr ${lr} --critic_lr ${critic_lr} --user_name ${wandb_user} --log_level ${log_level} --log_interval ${log_interval} #--use_eval --eval_interval ${eval_interval} --n_eval_rollout_threads ${n_eval_rollout_threads}
+    CUDA_VISIBLE_DEVICES=0 python train.py $@ --algorithm_name ${algo} --experiment_name ${experiment} --traffic_type ${traffic_type} --accelerate ${accelerate} --seed ${seed} --n_training_threads ${n_training_threads} --n_rollout_threads ${n_rollout_threads} --num_mini_batch ${num_mini_batch} --num_env_steps ${num_env_steps} --ppo_epoch ${ppo_epoch} --gain ${gain} --lr ${lr} --critic_lr ${critic_lr} --user_name ${wandb_user} --log_level ${log_level} --log_interval ${log_interval} --w_pc ${w_pc} --w_drop ${w_drop} --w_delay ${w_delay} #--use_eval --eval_interval ${eval_interval} --n_eval_rollout_threads ${n_eval_rollout_threads}
 done
