e8da38507e7f3a3004234643570c9651423cc47a
TZCai <13818704679@163.com>
2022-09-15 14:31:22 +0200

    refactor
    Former-commit-id: 2a71598d8139d8addc047721961c9b915bf04c0c [formerly 15227327cf39051f52ec1a7119ee09bff30b4cd4]
    Former-commit-id: c27b3f4590e9a16c80aa0bf60846a136a48ebc35

---
+++ traffic/model.py
@@ -0,0 +1,82 @@
+import enum
+import numpy as np
+import pandas as pd
+
+try:
+    from . import config
+    from utils import timeit
+except ImportError:
+    import config
+    timeit = lambda func: func
+
+
+class TrafficType(enum.IntEnum):
+    UNKNOWN = 0
+    A = 1
+    B = 2
+    C = 3
+    # D = 4
+
+class TrafficModel:
+    file_size = config.fileSize  # in bits
+    delay_budgets = config.delayBudgets
+    num_apps = config.numApps
+    app_names = config.appNames
+    period = 60 * 60 * 24 * 7  # a week (in seconds)
+    
+    profiles_path = config.profilesPath
+    sample_rate = config.dpiSampleRate
+
+    def __init__(self, area, period=period, scenario=None):
+        self.area = area[0] * area[1] / 1e6  # km^2
+        self.period = period
+        self.scenario = scenario
+
+    @classmethod
+    def from_scenario(cls, scenario, area):
+        if type(scenario) is str:
+            scenario = TrafficType[scenario.upper()]
+        else:
+            scenario = TrafficType(scenario)
+        profiles_df = pd.read_csv(cls.profiles_path, index_col=[0, 1, 2])
+        profiles_df /=  cls.sample_rate
+        profile = profiles_df.loc[int(scenario)]
+        return cls(area, scenario=scenario).fit(profile)
+
+    def fit(self, data_rate_df):
+        """ Fit the traffic model to the given traffic trace dataset. """
+        assert data_rate_df.shape[1] == self.num_apps
+        df = data_rate_df[self.app_names]
+        self.interval, rem = divmod(self.period, len(df))
+        assert rem == 0, (self.interval, rem)
+        self.rates = df * self.area / self.file_size  # files / s
+        assert self.rates.max().max() * 1e-3 <= 1.
+        return self
+    
+    @property
+    def time_slots(self):
+        return self.rates.index
+
+    @timeit
+    def emit_traffic(self, time, dt):
+        """ Randomly generate traffic for the given time.
+        
+        Returns:
+        A list of length n_apps, each element is either (traffic-demand, delay-budget) or False.
+        """
+        return [(self.file_size, delay) if np.random.rand() < p else (None, None)
+                for p, delay in zip(self.get_arrival_rates(time, dt), self.delay_budgets)]
+
+    def _get_time_loc(self, time):
+        return int((time / self.period) % 1 * len(self.rates))
+    
+    def get_time_slot(self, time):
+        i = self._get_time_loc(time)
+        return self.time_slots[i]
+    
+    def get_start_time_of_slot(self, time_slot):
+        return self.interval * self.time_slots.get_loc(time_slot)
+    
+    def get_arrival_rates(self, time, dt):
+        i = self._get_time_loc(time)
+        return self.rates.values[i] * dt  # (n_apps)
