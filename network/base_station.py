3005c567ac0f496e4b01af7923cd1c2fde1916f0
TZCai <13818704679@163.com>
2022-09-15 15:56:14 +0200

    update param
    Former-commit-id: aad7bc04e687244149bb2791279a01297af608e0 [formerly d4b39b00106d6c61150735c338c74e9c8763f2d5]
    Former-commit-id: 2af5bac8401ffe0312b9428c6b2f9ca7ded63760

--- network/base_station.py
+++ network/base_station.py
@@ -361,7 +361,7 @@ class BaseStation:
     @timeit
     def compute_power_consumption(
         self, eta=0.25, eps=8.2e-3, Ppa_max=3.125, Psyn=1,
-        Pbs=1, Pcd=0.9, Lbs=12.8, Tc=5000, Pfixed=18, C={},
+        Pbs=1, Pcd=1, Lbs=12.8, Tc=5000, Pfixed=18, C={},
         sm_deltas=config.sleepModeDeltas
     ):
         """
