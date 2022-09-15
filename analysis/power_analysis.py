6ee7e90a43d94945b7fe70482d6797463c39dd17
TZCai <13818704679@163.com>
2022-09-15 16:21:49 +0200

    update params
    Former-commit-id: 140d940effc2518b6aed61770c69f697744f76b2 [formerly 98e48beb91d41a417c584e06962e530e4f1b6453]
    Former-commit-id: 2e07c4d009382152b4bc08e5199e4c3a0ecb429b

---
+++ analysis/power_analysis.py
@@ -0,0 +1,24 @@
+# %%
+import numpy as np
+import pandas as pd
+import plotly.express as px
+
+stats_file = '../results/pc.txt'
+a = np.loadtxt(stats_file)
+df = pd.DataFrame(a, columns='M K R S Pnl Pld P'.split())
+df
+
+# %%
+px.scatter_3d(df.loc[df.S == 0],
+              x='M', y='K', z='Pld')
+# df.plot(kind='scatter', x='M', y='P')
+# df.plot(kind='scatter', x='K', y='Pld')
+# df.plot(kind='scatter', x='R', y='Pld')
+# df.plot(kind='scatter', x='S', y='Pnl')
+
+# %%
+px.scatter_3d(df.loc[df.S == 0],
+              x='M', y='R', z='Pld')
+
+# %%
+px.scatter_3d(df, x='S', y='M', z='Pnl')
--- traffic/config.py
+++ traffic/config.py
@@ -5,5 +5,5 @@ fileSize = 2 << 20  # file size in bits
 delayBudgets = [.05, .15, .30]  # in seconds
 appNames = ['Delay Stringent', 'Delay Sensitive', 'Delay Tolerant']
 # peakSumRate = 1 << 30  # maximum downlink sum rate in bps
-dpiSampleRate = 1/300  # probability of inspecting a traffic session in DPI data
+dpiSampleRate = 1/500  # probability of inspecting a traffic session in DPI data
 profilesPath = Path(__file__).parent.parent/'data/cluster_traffic_profiles.csv'
