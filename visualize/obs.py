04bc369cbf3e3a3dec55de508d18d854af36c605
TZCai <13818704679@163.com>
2022-09-19 22:21:19 +0200

    update
    Former-commit-id: 0ad61bb04f0aaeb6ec85d89bd40e96bbf9b06981 [formerly 5cd7571af35d0e8830a17cc524100d94634d9ad7]
    Former-commit-id: c3016c73768e7df0bf8c6d63f1fe12bd9f03e86e

--- visualize/obs.py
+++ visualize/obs.py
@@ -1,35 +1,34 @@
 import numpy as np
 import matplotlib.pyplot as plt
-from utils import trace_locals
+from utils import trace_locals, wraps
 
 
-class VisRolling:
+def anim_rolling(func):
     fig, ax = plt.subplots()
-    
-    def __new__(cls, func):
-        func = trace_locals(func)
-        def wrapped(self, *args, **kwargs):
-            ret = func(self, *args, **kwargs)
-            if not hasattr(self, 'id'):
-                cls.ax.cla()
-                self = func['self']
-                y = self._arrival_buf[[(self._buf_idx + i + 1) % self.buffer_ws
-                                       for i in range(self.buffer_ws)]]
-                cls.ax.plot(y)
-                plt.pause(0.001)
-            elif self.id == 0:
-                chunks = func['chunks']
-                arr = np.concatenate(chunks)
-                # plot chunks
-                cls.ax.cla()
-                cls.ax.plot(arr[:,0] / 100, label='pc')
-                cls.ax.plot(arr[:,1], label='traffic')
-                avg_arr = np.mean(chunks, axis=1)
-                xx = np.arange(0, arr.shape[0], chunks.shape[1])
-                cls.ax.plot(xx, avg_arr[:,0] / 100, label='avg_pc')
-                cls.ax.plot(xx, avg_arr[:,1], label='avg_traffic')
-                cls.ax.set_title('time: ' + str(self._time))
-                cls.ax.legend()
-                plt.pause(0.001)
-            return ret
-        return wrapped
+    func = trace_locals(func)
+    @wraps(func)
+    def wrapped(self, *args, **kwargs):
+        ret = func(self, *args, **kwargs)
+        if not hasattr(self, 'id'):
+            ax.cla()
+            self = func['self']
+            y = self._arrival_buf[[(self._buf_idx + i + 1) % self.buffer_ws
+                                   for i in range(self.buffer_ws)]]
+            ax.plot(y)
+            plt.pause(0.001)
+        elif self.id == 0:
+            chunks = func['chunks']
+            arr = np.concatenate(chunks)
+            # plot chunks
+            ax.cla()
+            ax.plot(arr[:,0] / 100, label='pc')
+            ax.plot(arr[:,1], label='traffic')
+            avg_arr = np.mean(chunks, axis=1)
+            xx = np.arange(0, arr.shape[0], chunks.shape[1])
+            ax.plot(xx, avg_arr[:,0] / 100, label='avg_pc')
+            ax.plot(xx, avg_arr[:,1], label='avg_traffic')
+            ax.set_title('time: ' + str(self._time))
+            ax.legend()
+            plt.pause(0.001)
+        return ret
+    return wrapped
