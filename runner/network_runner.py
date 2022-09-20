46d5308f1ba4ff8ed7942ebdc0d541b15d279891
tztsai <13818704679@163.com>
2022-09-20 20:03:13 +0000

    update training

--- runner/network_runner.py
+++ runner/network_runner.py
@@ -52,7 +52,7 @@ class MultiCellNetRunner(Runner):
                 rew_info.index = ['_'.join(idx) for idx in rew_info.index]
                 train_infos.update(rew_info)
                 avg_step_rew = np.mean(self.buffer.rewards)
-                assert np.allclose(avg_step_rew, train_infos['reward_mean'])
+                assert abs(avg_step_rew - train_infos['reward_mean']) < 1e-3
                 pbar.set_postfix(reward=avg_step_rew)
                 self.log_train(train_infos, total_num_steps)
 
