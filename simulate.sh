2d67ecdc91ed9771cb6860e29976add266c8ed3f
TZCai <13818704679@163.com>
2022-10-23 23:59:59 +0200

    update PC computation wrt M

--- simulate.sh
+++ simulate.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 for S in A B C; do
     for A in mappo fixed simple simple1 simple2; do
-        ./simulate.py -S $S -A $A --seed 42 -a 60000 #-V 40
+        ./simulate.py -S $S -A $A -a 60000 #-V 40
     done
 done
