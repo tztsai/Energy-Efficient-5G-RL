91be3e155ebd254983690b6aa74e8fcc1ca18a3b
TZCai <13818704679@163.com>
2022-10-23 21:45:20 +0200

    remove useless runs

--- clean.py
+++ clean.py
@@ -1,5 +1,5 @@
-import os
 import json
+import shutil
 from pathlib import Path
 
 run_results_dir = Path('results/MultiCellNetwork')
@@ -8,6 +8,7 @@ run_dirs = run_results_dir.glob('*/*/*/wandb/run*/')
 for d in run_dirs:
     with open(d / 'files/wandb-summary.json') as f:
         s = json.load(f)
-    if 'reward_mean' not in s:
+    if 'reward_mean' not in s or s['_step'] < 1e6:
         print(d)
+        shutil.rmtree(str(d))
         
