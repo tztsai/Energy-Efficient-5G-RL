f0a80265c974860320546d04793025111194f805
TZCai <13818704679@163.com>
2022-10-26 05:02:05 +0200

    update

--- simulate.sh
+++ simulate.sh
@@ -3,10 +3,10 @@ seed=1
 acc=3000
 
 for S in A B C; do
-    for w in 8; do
+    for w in 4 8; do
         ./simulate.py -S $S --w_qos $w --seed $seed -a $acc $@
     done
-    # for A in fixed simple simple1 simple2; do
-    #     ./simulate.py -S $S -A $A --seed $seed -a $acc $@
-    # done
+    for A in fixed simple simple1 simple2; do
+        ./simulate.py -S $S -A $A --seed $seed -a $acc $@
+    done
 done
