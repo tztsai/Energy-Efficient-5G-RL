dd8efb960591152fa9f5e7a1c8df09e3c5d1ca0e
tztsai <13818704679@163.com>
2022-10-28 04:33:53 +0000

    update training

--- train.sh
+++ train.sh
@@ -3,16 +3,16 @@ seed_max=1
 log_level="NOTICE"
 
 scenario="RANDOM"
-accelerate=1500  # 1 step = 0.02 * 1500 = 30 s
+accelerate=1200  # 1 step = 0.02 * 1200 = 24 s
 n_training_threads=4
 n_rollout_threads=42
-num_env_steps=$((20160 * 50))  # steps_per_episode * episodes
+num_env_steps=$((25200 * 60))  # steps_per_episode * episodes
 experiment="check"
 
 algo="mappo"
 gamma=0.99
 gain=0.01
-lr=6e-4
+lr=5e-4
 critic_lr=$lr
 value_loss_coef=1
 ppo_epoch=10
