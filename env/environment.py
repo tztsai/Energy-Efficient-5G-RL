9a6e36bc6ebb3a570ececeab5feacadfe6922fbe
tztsai <13818704679@163.com>
2022-12-05 10:17:23 +0000

    update

--- env/environment.py
+++ env/environment.py
@@ -241,7 +241,7 @@ class MultiCellNetEnv(MultiAgentEnv):
     def close(self):
         if EVAL:
             stats_dir = os.path.join(
-                self.stats_dir, self.net.traffic_model.scenario.name, f'SEED{self.seed}')
+                self.stats_dir, self.net.traffic_model.scenario.name, f'SEED{self._seed}')
             os.makedirs(stats_dir, exist_ok=True)
             self.net.save_stats(stats_dir)
             print('Stats saved to', stats_dir)
