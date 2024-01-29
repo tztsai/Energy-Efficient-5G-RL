d81ef7156906b0a0c9c7929f477bc699a734637c
TZCai <13818704679@163.com>
2024-01-29 16:27:34 +0000

    fix train.sh

--- train.sh
+++ train.sh
@@ -24,17 +24,15 @@ num_mini_batch=1
 
 w_qos=4
 w_xqos=0.005
-# w_drop=0.5
-# w_delay=0.1
 
 log_interval=1
 
-wandb_user="tcai7"
-wandb_api_key="e3662fa8db0f243936c7514a1d0c69f2374ce721"
+wandb_user="YOUR_WANDB_USER_NAME"
+wandb_api_key="YOUR_WANDB_API_KEY"
 
 echo "algo is ${algo}, traffic scenario is ${scenario}, max seed is ${seed_max}"
 for seed in `seq ${seed_max}`;
 do
     echo "seed is ${seed}:"
-    CUDA_VISIBLE_DEVICES=0 python train.py --algorithm_name ${algo} --experiment_name ${experiment} --scenario ${scenario} --accelerate ${accelerate} --seed ${seed} --n_training_threads ${n_training_threads} --n_rollout_threads ${n_rollout_threads} --num_mini_batch ${num_mini_batch} --num_env_steps ${num_env_steps} --ppo_epoch ${ppo_epoch} --gain ${gain} --gamma ${gamma} --lr ${lr} --critic_lr ${critic_lr} --value_loss_coef ${value_loss_coef} --user_name ${wandb_user} --log_level ${log_level} --log_interval ${log_interval} --w_qos ${w_qos} --w_xqos ${w_xqos} $@ #--use_eval --eval_interval ${eval_interval} --n_eval_rollout_threads ${n_eval_rollout_threads}
+    CUDA_VISIBLE_DEVICES=0 python train.py --algorithm_name ${algo} --experiment_name ${experiment} --scenario ${scenario} --accelerate ${accelerate} --seed ${seed} --n_training_threads ${n_training_threads} --n_rollout_threads ${n_rollout_threads} --num_mini_batch ${num_mini_batch} --num_env_steps ${num_env_steps} --ppo_epoch ${ppo_epoch} --gain ${gain} --gamma ${gamma} --lr ${lr} --critic_lr ${critic_lr} --value_loss_coef ${value_loss_coef} --user_name ${wandb_user} --log_level ${log_level} --log_interval ${log_interval} --w_qos ${w_qos} --w_xqos ${w_xqos} $@
 done
